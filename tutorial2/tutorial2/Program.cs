e1b4cbbb03893511c6915c074330f3679d0f8263
shurikandrew <s26865@pjwstk.edu.pl>
2024-03-14 23:00:06 +0100

    Average function Final

--- tutorial2/tutorial2/Program.cs
+++ tutorial2/tutorial2/Program.cs
@@ -1,4 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿static double average(int[] arr)
+{
+    double result = 0.0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        result += arr[i];
+    }
 
-Console.WriteLine("Goodbye, World!");
-Console.WriteLine("Hello");
+    result /= arr.Length;
+    return result;
+}
+
+int[] temp = {1,2,3,4,5,6,7,8,9};
+double result = average(temp);
+Console.WriteLine(result);
