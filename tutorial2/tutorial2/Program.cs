75f5e95a50f920e77f7951f56252b554449f341e
shurikandrew <s26865@pjwstk.edu.pl>
2024-03-14 23:37:48 +0100

    Added max function

--- tutorial2/tutorial2/Program.cs
+++ tutorial2/tutorial2/Program.cs
@@ -10,6 +10,21 @@
     return result;
 }
 
+static int maxElement(int[] arr)
+{
+    int result = arr[0];
+    for (int i = 1; i < arr.Length; i++)
+    {
+        if (result < arr[i])
+        {
+            result = arr[i];
+        }
+    }
+    return result;
+}
+
 int[] temp = {1,2,3,4,5,6,7,8,9};
 double result = average(temp);
+int result2 = maxElement(temp);
 Console.WriteLine(result);
+Console.WriteLine(result2);
